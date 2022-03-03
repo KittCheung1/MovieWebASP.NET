f1c99db24b2186e6df1f9bb69672d67e7cb75aa8
Kitt Cheung <kitt.cheung@live.se>
2022-03-03 10:50:24 +0100

    FEAT: added a fanchiseprofile

---
+++ Profiles/FranchiseProfile.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using TestWebASP.NET.DTO.Franchise;
+using TestWebASP.NET.Models;
+
+namespace TestWebASP.NET.Profiles
+{
+    public class FranchiseProfile : Profile
+    {
+        public FranchiseProfile()
+        {
+            CreateMap<Franchise, ReadFranchiseDTO>()
+                    .ReverseMap();
+
+            CreateMap<Franchise, CreateFranchiseDTO>()
+                    .ReverseMap();
+
+        }
+    }
+}
